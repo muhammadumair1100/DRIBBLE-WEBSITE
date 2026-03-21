fc9223ab15f09265b14b1496396cb0554e14c7fd
Muhammad Umair <muhammadumair47379@gmail.com>
2026-03-21 23:16:25 +0500

    fixed deploy error

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { X } from "lucide-react";
 // Website Home Pages Components
 import Home from "./components/Home/Home";
 // signup & login pages
 import Signup from "./components/pages/SignupLogin/Signup";
 import Shots from "./components/Home/Shots";
+
 // HireTalent Pages
 import StartProjectBrief from "./components/pages/NavbarMenuItem/HireTalent/startProjectBrief/StartProjectBrief";
-import BrowseProfiles from "./components/pages/NavbarMenuItem/HireTalent/BrowseProfiles/BrowseProfiles";
+import BrowseProfiles from "./components/pages/NavbarMenuItem/HireTalent/BrowseProfiles/BrowseProfile";
 import SelectedCard from "./components/pages/HomeCards/SelectedCard";
 import ForDesigners from "./components/pages/FooterPagess/ForDesigners/ForDesigners";
 import Services from "./components/pages/HeroSection/Search";
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route, Link, useParams } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Navbar from "./Navbar";
 import Hero from "./Hero";
 import Filters from "./Filters";
@@ -8,6 +8,7 @@ import HomeCards from "./HomeCards";
 import Marque from "./Marque";
 import Footer from "./Footer";
 import SelectedCard from "../pages/HomeCards/SelectedCard";
+import { X } from "lucide-react";
 const Home = () => {
   const [selectedCard, setSelectedCard] = useState(false);
 
